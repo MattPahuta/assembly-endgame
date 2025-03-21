fa74a1ab5b19cb3bc4c7f640516e67827ffcdf58
Matt Pahuta <mhpahuta@gmail.com>
2025-03-21 10:23:27 -0700

    Add keyboard styling and functionality, update base styles

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,6 +5,18 @@ import LanguageChips from './LanguageChips';
 function AssemblyEndgame() {
 
   const [currentWord, setCurrentWord] = React.useState('react');
+  const [guessedLetters, setGuessedLetters] = React.useState([]);
+
+  console.log(guessedLetters);
+
+  function addGuessedLetter(letter) {
+    // Add the clicked letter to the guessedLetters array
+    // Check if the clicked letter is in the currentWord
+    setGuessedLetters(prevLetters => (
+      prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter]
+      )
+    )
+  }
 
   const letterElements = currentWord.split('').map((letter, index) => {
     return (
@@ -17,7 +29,7 @@ function AssemblyEndgame() {
   const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
   const keyboardElements = alphabet.split('').map((letter, index) => (
-    <button key={index} className="keyboard-key">
+    <button key={index} onClick={() => addGuessedLetter(letter)} className="button keyboard-button">
       {letter.toUpperCase()}
     </button>
   ));
@@ -36,6 +48,7 @@ function AssemblyEndgame() {
           {keyboardElements}
         </div>
       </section>
+      <button className="button new-game-button">New Game</button>
     </div>
   )
 }
