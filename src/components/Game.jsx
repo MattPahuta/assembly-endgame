dff9bb603f25354f310f0956f3da8a19d6dd3d47
Matt Pahuta <mhpahuta@gmail.com>
2025-03-20 17:02:07 -0700

    Add word and keyboard elements

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,11 +1,41 @@
+import React from 'react';
 import Banner from './Banner';
 import LanguageChips from './LanguageChips';
 
 function AssemblyEndgame() {
+
+  const [currentWord, setCurrentWord] = React.useState('react');
+
+  const letterElements = currentWord.split('').map((letter, index) => {
+    return (
+      <p key={index} className="letter">
+        {letter.toUpperCase()}
+      </p>
+    )
+  })
+
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+  const keyboardElements = alphabet.split('').map((letter, index) => (
+    <button key={index} className="keyboard-key">
+      {letter.toUpperCase()}
+    </button>
+  ));
+
   return (
     <div className="game-wrapper">
       <Banner />
       <LanguageChips />
+      <section className="word-section">
+        <div className="word-container">
+        {letterElements}
+        </div>
+      </section>
+      <section className="keyboard-section">
+        <div className="keyboard">
+          {keyboardElements}
+        </div>
+      </section>
     </div>
   )
 }
